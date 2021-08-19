d1bb9af5f1d9c216bdf81e7494908ca33c98bb27
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-18 20:52:27 -0500

    token

--- strtok.c
+++ strtok.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 #include "shell.h"
 
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
+#define TOK_BUFSIZE 1024
+#define TOK_DELIM " \t\r\n\a"
 
-char **main(char *line)
+char **tokenization(char *line)
 
 {
-	char text[] = "hola:mundo:chao:chicos";
-	const char *DELIM = ":";
 	char *token;
-	int bufsize =  LSH_TOK_BUFSIZE, p = 0;
-	char **tokens = malloc(bufsize *sizeof(char*));
+	int bufsize =  TOK_BUFSIZE, p = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
 
-	token = strtok(text, DELIM);
+	if (!tokens)
+		exit(EXIT_SUCCESS);
+
+	token = strtok(line, TOK_DELIM);
 
 	while (token != NULL)
 	{
@@ -22,11 +23,9 @@ char **main(char *line)
 		p++;
 
 		printf("%s\n", token);
-		token = strtok(NULL, DELIM);
+		token = strtok(NULL, TOK_DELIM);
 		/*token = strtok(NULL, LSH_TOK_DELIM);*/
 	}
 	tokens[p] = NULL;
 	return (tokens);
-    //ls -a -l ->tokenizar!
-    //bin/usr:bin:games/usr ->tokenizar!
 }
