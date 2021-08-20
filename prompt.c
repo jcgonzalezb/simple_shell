8793cee3f9fdce9a12f1b04e5e6ba348b39fc438
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-19 21:12:57 -0500

    prompt

--- prompt.c
+++ prompt.c
@@ -13,18 +13,29 @@ void prompt(void)
 {
 	char *line;
 	char *prompt = "$ ";
-
 	char **args;
 	/*int info;*/
+	void (*builtin) (char**);
+
 	do {
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, prompt, _strlen(prompt));
 
 		line = read_c();
 		args = tokenization(line);
+
+		/*if (args[0] == NULL)*/
+		/*	return (1);*/
+
+		builtin = selectfunction(args);
+
+		if (builtin == NULL)
+			printf("Search in the path");
+				
 		/*info = execute(args);*/
 
 		/*free(line);*/
 		/*free(args);*/
+
 	} while (1);
 }
