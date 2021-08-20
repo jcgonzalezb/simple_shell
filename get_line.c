439351b5732acd59506364bc82dbf8ce20fee56b
PablitoOsorix <3387@holbertonschool.com>
2021-08-20 11:34:55 -0500

    This is our read (with getline) function.

---
+++ get_line.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "shell.h"
+
+/**
+ * prompt - prompter.
+ * @data: data
+ * Return: Result
+ */
+char *read_c(void)
+{
+	char *line = NULL;
+	ssize_t lineSize = 0;
+
+	if (getline(&line, &lineSize, stdin) == -1)
+		exit(EXIT_SUCCESS);
+
+	/*charactersread = getline(&line, &lineSize, stdin);+/
+	  /* write(STDOUT_FILENO, line, charactersread); */
+	/*tokens*/
+	return (line);
+}
