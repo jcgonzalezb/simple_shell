66dde8f5370207c3de4c8ba7c64b71e68257fd2d
PablitoOsorix <3387@holbertonschool.com>
2021-08-20 11:33:02 -0500

    This is our header file.

--- shell.h
+++ shell.h
@@ -1,12 +1,26 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdarg.h>
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 int _putchar(char c);
 int _strlen(char *s);
 char **_strtok(char *line);
+void prompt(void);
+char *read_c(void);
+char **tokenization(char *line);
+void (*selectfunction(char **args))(char **args);
+int _strcmp(char *s1, char *s2);
+void exitf(char **args);
+
+typedef struct buildin
+{
+	char *program;
+	void (*f)(char **args);
+} order;
+
 #endif
