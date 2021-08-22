ec4505d945d15468f4c36f9430c34d95b3d780dd
PablitoOsorix <3387@holbertonschool.com>
2021-08-21 21:19:33 -0500

    I have updated our header.

--- shell.h
+++ shell.h
@@ -6,7 +6,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
+extern char **environ;
+extern char **environ;
 int _putchar(char c);
 int _strlen(char *s);
 char **_strtok(char *line);
@@ -16,6 +17,11 @@ char **tokenization(char *line);
 void (*selectfunction(char **args))(char **args);
 int _strcmp(char *s1, char *s2);
 void exitf(char **args);
+char *getvarfromenv(char *varname);
+char **tokenizepath(char *string);
+char *_strncat(char *dest, char *src);
+char *_strdup(char *str);
+int stat_command(char **args, char **dividedPath);
 
 typedef struct buildin
 {
