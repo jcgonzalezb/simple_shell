072db95807af699d46a0a64076cfb40828b7f883
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-24 18:20:51 -0500

    stat file

--- stat_command.c
+++ stat_command.c
@@ -9,10 +9,9 @@
  */
 int stat_command(char **args, char **dividedPath)
 {
-	int i = 0, j = 0, x = 0;
+	int i = 0, j = 0, x = 0, execute = 0, status = 0, pid = 0;
 	char buffer[1024];
 	struct stat sb;
-	int execute = 0, status = 0, pid = 0;
 
 	for (i = -1; i == -1 || dividedPath[i] != NULL; i++)
 	{
@@ -27,7 +26,6 @@ int stat_command(char **args, char **dividedPath)
 		if ((stat(buffer, &sb) == 0) && j != 1)
 		{
 			pid = fork();
-
 			if (pid == -1)
 			{
 				perror("Error:");
