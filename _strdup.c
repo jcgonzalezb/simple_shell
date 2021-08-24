4ce583475f5a2662da753d021fcdfa1ed1aae03e
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-23 22:42:23 -0500

    strdup file

--- _strdup.c
+++ _strdup.c
@@ -1,36 +1,27 @@
-#include "shell.c"
+#include "shell.h"
 
 /**
- * _strdup - returns a pointer to a newly allocated
- * space in memory, which contains a copy of the string
- * given as a parameter.
- * @str : String
- * Return: Returns a pointer to the duplicated string.
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: the string
+ * Return: the pointer
  */
-
 char *_strdup(char *str)
 {
-	char *duplicate_str;
-	int i = 0, len = 0;
+	int i, j;
+	char *dest;
 
 	if (str == NULL)
 		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
-	while (*(str + i))
-		len++, i++;
-		len++;
-
-	duplicate_str = malloc(sizeof(char) * len);
-
-	if (duplicate_str == NULL)
+	dest = malloc((i * sizeof(str[0])) + 1);
+	if (dest == NULL)
 		return (NULL);
+	for (j = 0; j <= i; j++)
+		dest[j] = str[j];
 
-	i = 0;
-	while (i < len)
-	{
-		*(duplicate_str + i) = *(str + i);
-		i++;
-	}
-
-	return (duplicate_str);
+	return (dest);
+	free(dest);
 }
