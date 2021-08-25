7a94ebed8e0973baf067c39f3109e8dc5c43bd77
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-25 00:33:46 -0500

    error file

--- error.c
+++ error.c
@@ -9,6 +9,7 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0; /* couters to arrays */
+
 	while (dest[i] != '\0')
 		i++;
 	for (; (dest[i] = src[j]) != '\0';)
@@ -22,8 +23,8 @@ char *_strcat(char *dest, char *src)
 /**
  * _perror - This function do the stat, PID & PPID,
  * and find & excecute the command in the path.
- * @args: This is the input command tokenaized yet.
- * @dividedPath: These are all the paths tokenaized yet.
+ * @shell_name: Name of the shell.
+ * @command_name: Name of the command.
  * Return: Zero in case completes the task successfully.
  */
 
@@ -31,13 +32,14 @@ void _perror(char *shell_name, char *command_name)
 {
 	int i = 0;
 	char error[1024];
+
 	_strcat(error, shell_name);
 	_strcat(error, ": ");
 	_strcat(error, "1: ");
 	_strcat(error, command_name);
 	_strcat(error, ": ");
 	_strcat(error, "not found\n");
-	
+
 	for (i = 0; error[i] != '\0'; i++)
 	{}
 
